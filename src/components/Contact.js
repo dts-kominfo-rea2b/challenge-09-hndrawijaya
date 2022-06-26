30d130cb62b8510fe57bef115145ed8ec4a2124d
Hendra Wijaya <wijaya.hndra@gmail.com>
2022-06-26 10:47:58 +0700

    Done Intro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,17 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    const {data} = props;
     return (
-        <></>
+        <div className="card">
+        <img src={data.photo} alt="broken" />
+        <div>
+            <h1>{data.name}</h1>
+            <h2>{data.phone}</h2>
+            <h3>{data.email}</h3>
+        </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <header>
+            <h1 className='title'>Call a Friend</h1>
+            <p className='subtitle'>your friendly contact app</p>
+            <hr/>
+        </header>
     )
 }
 
